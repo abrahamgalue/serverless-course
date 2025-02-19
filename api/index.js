0090cc87307af5b548e36f2a19654104d69434db
Abraham Galue <abrahamgalue512@gmail.com>
2025-02-19 16:14:43 -0400

    Middleware added for parsing JSON files coming with Express

--- api/index.js
+++ api/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const { json } = require('express')
 const mongoose = require('mongoose')
 const meals = require('./routes/meals')
 const orders = require('./routes/orders')
+
 const app = express()
+app.use(json())
 
 mongoose.connect(process.env.MONGODB_URI)
 
