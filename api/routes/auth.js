4c73d4e1f10bb46e265ef19471602887c46aefb6
Abraham Galue <abrahamgalue512@gmail.com>
2025-02-24 14:01:45 -0400

    Created log route

---
+++ api/routes/auth.js
@@ -0,0 +1,35 @@
+const { Router } = require('express')
+const crypto = require('node:crypto')
+const Users = require('../models/Users')
+
+const router = Router()
+
+router.post('/register', (req, res) => {
+  const { email, password } = req.body
+  crypto.randomBytes(16, (err, salt) => {
+    const newSalt = salt.toString('base64')
+    crypto.pbkdf2(password, newSalt, 10000, 64, 'sha1', (err, key) => {
+      const encryptedPassword = key.toString('base64')
+      Users.findOne({ email }).exec()
+        .then(user => {
+          if (user) {
+            return res.send('usuario ya existe')
+          }
+          Users.create({
+            email,
+            password: encryptedPassword,
+            salt: newSalt
+          }).then(() => {
+            res.send('usuario creado con exito')
+          })
+        })
+    })
+  })
+})
+
+router.post('/login', (req, res) => {
+  res.send('soy login')
+})
+
+
+module.exports = router
